3df4f98771ba114bde2f84b0c9310ec89ef67079
Hai LE <69892340+lehaihv@users.noreply.github.com>
2024-11-13 16:45:33 -0500

    Mac 7740

---
+++ Cpp/chap1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+    {
+        cout << "Every age has a language of its own\n";
+        return 0;
+    }
